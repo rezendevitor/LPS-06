ee22cd27b9db851f67a953bb26bc9227056a75b6
rezendevitor <55188161+rezendevitor@users.noreply.github.com>
2022-02-20 11:30:50 -0300

    commit

---
+++ LPS06/src/main/java/lps06/LPS06.java
@@ -0,0 +1,10 @@
+package lps06;
+
+import java.text.ParseException;
+import view.FrTelaPrincipal;
+
+public class LPS06 {
+    public static void main(String[] args) throws ParseException {
+        FrTelaPrincipal telaPrincipal = new FrTelaPrincipal();
+    }
+}
